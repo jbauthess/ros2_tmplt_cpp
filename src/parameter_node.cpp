13076965bae90fa60f5d497661e03e305f325f74
Jean-Baptiste AUTHESSERRE <jean-baptiste.authesserre.ext@safrangroup.com>
2026-06-02 15:58:06 +0200

    Add parameter change at run-time

--- src/parameter_node.cpp
+++ src/parameter_node.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/string.hpp"
 
+#define PARAM_NAME "name"
 
 class ParameterNode : public rclcpp::Node
 {
@@ -12,11 +13,20 @@ class ParameterNode : public rclcpp::Node
           m_Publisher = this->create_publisher<example_interfaces::msg::String>("print_name", 10);
 
           // declare parameter
-          this->declare_parameter("name", "R2D2");
+          this->declare_parameter(PARAM_NAME, "R2D2");
 
 
           // get parameter value
-          m_param_name = this->get_parameter("name").as_string();
+          m_param_name = this->get_parameter(PARAM_NAME).as_string();
+
+          // Set a callback to modify parameter at run-time
+          m_ParamCallbackOwner= this->add_post_set_parameters_callback(
+            std::bind(
+            &ParameterNode::postSetParameterCallback, 
+            this,
+            std::placeholders::_1
+            )
+          );
 
        }
 
@@ -28,9 +38,22 @@ class ParameterNode : public rclcpp::Node
             m_Publisher->publish(Msg);
        }
 
+       void
+         postSetParameterCallback(const std::vector<rclcpp::Parameter> & parameters)
+         {
+         for(const auto & param:parameters) {
+            // the internal class member can be changed after
+            // successful change to param1 or param2
+            if(param.get_name() == PARAM_NAME) {
+               m_param_name = param.get_value<std::string>();
+            }
+         }
+ }
+
        rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr m_Publisher;
        rclcpp::TimerBase::SharedPtr m_Timer;
        std::string m_param_name;
+       rclcpp::node_interfaces::PostSetParametersCallbackHandle::SharedPtr m_ParamCallbackOwner;
 };
 
 int main(int argc, char ** argv)
@@ -41,3 +64,8 @@ int main(int argc, char ** argv)
     rclcpp::shutdown();
     return 0;
 }
+
+// Test this code:
+// > ros2 param set /parameter name "C3POOOO"
+// > ros2 param get /parameter name
+// > ros2 topic echo /print_name
